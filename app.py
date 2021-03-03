3e17b3fc1ef91eac2dba5b0f84792daa000da9e1
Trey Annis <ta08annis@gmail.com>
2021-03-03 13:15:25 -0500

    More changes and revisions, testing.

---
+++ app.py
@@ -0,0 +1,6 @@
+from game import Game
+from phrase import Phrase
+
+if __name__ == "__main__":
+    new_game = Game()
+    new_game.start()
---
+++ game.py
@@ -0,0 +1,59 @@
+import random
+from phrase import Phrase
+class Game:
+
+    def __init__(self):
+        self.missed = 0
+        # -- https://randomwordgenerator.com/ -- 
+        self.phrases = ["price", "board", "court", "spoil", "graze"]
+        self.active_phrase = None
+        self.guesses = []
+        
+
+    def start(self):
+        # Calls the welcome method, creates the game loop, calls the get_guess method, adds the user's guess to guesses, 
+        # increments the number of missed by one if the guess is incorrect, calls the game_over method.
+        self.welcome()
+        random_phrase = self.get_random_phrase()
+        print(random_phrase)
+        new_phrase = Phrase(random_phrase)
+        
+        while True:
+            user_guess = self.guesses.append(self.get_guess())
+            print(self.guesses)
+            new_phrase.display_table
+            if new_phrase.check_letter(user_guess) == False:
+                self.missed += 1
+                if self.missed == 5:
+                    self.game_over()
+                    
+
+
+
+    def get_random_phrase(self):
+        # this method randomly retrieves one of the phrases stored in the phrases list and returns it.
+        return random.choice(self.phrases)
+
+    def welcome(self):
+        print("""
+
+    ╭━━━╮╱╱╱╭╮╭╮╱╱╱╱╱╱╱╱╭━━━━╮╱╱╱╱╭╮╱╱╱╭╮╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭╮╱╭╮╱╱╱╭╮╱╭━━━╮
+    ┃╭━╮┃╱╱╭╯╰┫┃╱╱╱╱╱╱╱╱┃╭╮╭╮┃╱╱╱╱┃┃╱╱╱┃┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱┃┃╱┃┃╱╱╭╯╰╮┃╭━╮┃
+    ┃╰━╯┣╮╱┣╮╭┫╰━┳━━┳━╮╱╰╯┃┃┣┻━┳━━┫╰━┳━╯┣━━┳━━┳━┳━━┳━━╮┃┃╱┃┣━╮┣╮╭╯╰╯╭╯┃
+    ┃╭━━┫┃╱┃┃┃┃╭╮┃╭╮┃╭╮╮╱╱┃┃┃┃━┫╭━┫╭╮┃╭╮┃┃━┫╭╮┃╭┫┃━┫┃━┫┃┃╱┃┃╭╮╋┫┃╱╭━╯╭╯
+    ┃┃╱╱┃╰━╯┃╰┫┃┃┃╰╯┃┃┃┃╱╱┃┃┃┃━┫╰━┫┃┃┃╰╯┃┃━┫╰╯┃┃┃┃━┫┃━┫┃╰━╯┃┃┃┃┃╰╮┃┃╰━╮
+    ╰╯╱╱╰━╮╭┻━┻╯╰┻━━┻╯╰╯╱╱╰╯╰━━┻━━┻╯╰┻━━┻━━┻━╮┣╯╰━━┻━━╯╰━━━┻╯╰┻┻━╯╰━━━╯
+    ╱╱╱╱╭━╯┃╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╭━╯┃
+    ╱╱╱╱╰━━╯╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰━━╯                                                                                                                                                                                                
+    """, "\n")
+        print("== Welcome to Phrase Hunters. ==")
+
+    def get_guess(self):
+        user_guess = input("Guess a letter: ")
+        return user_guess
+
+
+    def game_over(self):
+        print("you have failed humanity.")
+        exit(0)
+    
